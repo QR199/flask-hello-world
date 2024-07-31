2ea77e5e558ce5f34374d37b667c4bdb14d9d855
Quinn Ridgeway <quri8255@colorado.edu>
2024-07-31 21:45:22 +0000

    psycopg2 database route

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask
+import psycopg2
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World from Quinn Ridgeway 3308!'
+
+@app.route('/db_test')
+def testing():
+    conn = psycopg2.connect("postgresql://quinn_lab_10_user:8qOkcdHE9bBlNyxrNFm9RWbqfj4uaHlu@dpg-cqlat6pu0jms7389hs0g-a/quinn_lab_10")
+    conn.close()
+    return "Database Connection Successful"
